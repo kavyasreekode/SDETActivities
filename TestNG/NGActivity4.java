53e8432f33e75039a583218b9d55076c16de3f7e
kavyasreekode <75238512+kavyasreekode@users.noreply.github.com>
2020-12-22 10:08:17 +0530

    Add files via upload

---
+++ TestNG/NGActivity4.java
@@ -0,0 +1,11 @@
+package TestNGActivities;
+
+import org.testng.annotations.Test;
+
+
+public class NGActivity4 {
+    @Test
+    public void TestCase() {
+        System.out.println("I'm in the test case from DemoTwo Class");
+    }
+}
